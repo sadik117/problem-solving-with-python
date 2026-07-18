fa0e92c56971c0b580d9bf6dc99cad92fb4bc567
unknown <sadiksourov11@gmail.com>
2026-07-19 00:03:23 +0600

    star pattern problem solved

--- problem-4.py
+++ problem-4.py
@@ -8,8 +8,8 @@
 rows = 3
 
 for i in range(rows):
-    for j in range(rows - i - 1):
+    for j in range(rows - i + 1):
       print(" ", end="")
-    for k in range(2* i + 1):  
+    for k in range(2 * i + 1):  
       print("*", end="")
     print()
