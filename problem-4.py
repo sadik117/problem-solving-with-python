2c270f6d4db817ce6d3547794996d12b84ef1daa
unknown <sadiksourov11@gmail.com>
2026-07-18 23:49:27 +0600

    star pattern problem solved

---
+++ problem-4.py
@@ -0,0 +1,15 @@
+# Problem 4:Write a program to pring the following pattern
+
+#     *
+#   * * *
+# * * * * *
+
+
+rows = 3
+
+for i in range(rows):
+    for j in range(rows - i - 1):
+      print(" ", end="")
+    for k in range(2* i + 1):  
+      print("*", end="")
+    print()
