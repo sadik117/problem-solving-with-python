ef606532183f09598b87683db483fbc0e2e7b9a6
unknown <sadiksourov11@gmail.com>
2026-09-10 15:37:50 +0600

    namespace topics problem solved

---
+++ problem-17-namespace-topics.py
@@ -0,0 +1,32 @@
+# Q1: Write Person Class as given below and then display it's namespace.
+
+# Class Name - Person
+
+# Attributes:
+# name - public
+# state - public
+# city - private
+# age - private
+
+# Methods:
+# address - public
+# It give address of the person as "<name>, <city>, <state>"
+
+
+
+class Person:
+
+    def __init__(self, name, state, city, age):
+        self.name = name
+        self.state = state
+        self.__city = city
+        self.__age = age
+
+    def address(self):
+        return f"{self.name}, {self.__city}, {self.state}"
+
+
+p1 = Person('Sadik', 'Bangladesh', 'Rajshahi', 22)
+
+print(p1.address())
+print(Person.__dict__)
